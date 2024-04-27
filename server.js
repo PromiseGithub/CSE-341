83677f1a1e86b9e94d5c9a5cb0bf918bc3932b16
PromiseGithub <73972260+PromiseGithub@users.noreply.github.com>
2024-04-27 17:41:30 -0600

    going above and beyond for 100% on the first assignment :)

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send("Sarah Birch");
-});
 
 const port = 3000;
+
+app.use('/', require('./routes'))
  
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
